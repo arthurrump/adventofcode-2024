391209d62234b1685d1d7b02f08ae19dc02f1fd3
Arthur Rump <hello@arthurrump.com>
2024-12-02 21:21:00 +0100

    Day 2 part 2

--- Day02/Program.fs
+++ Day02/Program.fs
@@ -19,3 +19,14 @@ let part1 () =
     data |> Array.filter isSafe |> Array.length
 
 printfn "Part 1: %A" (part1 ())
+
+let isSafeDampened report =
+    Seq.init (Array.length report) id
+    |> Seq.exists (fun i -> isSafe (Array.removeAt i report))
+
+let part2 () =
+    data 
+    |> Array.filter (fun report -> isSafe report || isSafeDampened report)
+    |> Array.length
+
+printfn "Part 2: %A" (part2 ())

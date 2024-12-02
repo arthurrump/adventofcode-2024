081d5b1e35f87b5e2e28b32aa63eeea2780819a8
Arthur Rump <hello@arthurrump.com>
2024-12-02 21:16:43 +0100

    Day 2 part 1

---
+++ Day02/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.IO
+
+let parseLine (line: string) =
+    line.Split(" ")
+    |> Array.map int
+
+let data =
+    File.ReadAllLines("input.txt")
+    |> Array.map parseLine
+
+let isSafe report =
+    let pairs = Array.pairwise report
+    let increasing = Array.forall (fun (a, b) -> a < b) pairs
+    let decreasing = Array.forall (fun (a, b) -> a > b) pairs
+    let properDist = Array.forall (fun (a, b) -> abs (a - b) <= 3) pairs
+    (increasing || decreasing) && properDist
+
+let part1 () =
+    data |> Array.filter isSafe |> Array.length
+
+printfn "Part 1: %A" (part1 ())

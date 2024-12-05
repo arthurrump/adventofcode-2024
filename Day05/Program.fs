afd4f60b4fcfa6266a569414bf163a26440d4553
Arthur Rump <hello@arthurrump.com>
2024-12-05 19:28:36 +0100

    Day 5 part 1

---
+++ Day05/Program.fs
@@ -0,0 +1,45 @@
+﻿open System.IO
+
+let input = File.ReadAllLines("input.txt")
+
+let rules =
+    input 
+    |> Array.takeWhile (fun line -> line.Contains('|'))
+    |> Array.map (fun line -> 
+        let [| left; right |] = line.Split('|')
+        int left, int right
+    )
+
+let compare rules a b =
+    if Array.contains (a, b) rules
+    then -1
+    elif Array.contains (b, a) rules
+    then 1
+    else 0
+
+let updates =
+    input 
+    |> Array.skipWhile (fun line -> not (line.Contains(',')))
+    |> Array.map (fun line ->
+        line.Split(',')
+        |> Array.map int
+    )
+
+let isInOrder (rules: Map<int, int>) comp (update: int array) =
+    Array.indexed update
+    |> Array.forall (fun (i, page) ->
+        match Map.tryFind page rules with
+        | Some after ->
+            match Array.tryFindIndex (fun p -> p = after) update with
+            | Some afterI -> comp i afterI
+            | None -> true
+        | None -> true 
+    )
+
+let part1 () =
+    updates 
+    |> Array.filter (fun update ->
+        update = Array.sortWith (compare rules) update)
+    |> Array.sumBy (fun update -> update[Array.length update / 2])
+
+printfn "Part 1: %A" (part1 ())

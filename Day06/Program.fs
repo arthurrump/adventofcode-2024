12a5ef236e3aa03ca8e265c21b30ccbd86df25c9
Arthur Rump <hello@arthurrump.com>
2024-12-06 10:13:29 +0100

    Day 6 part 2 (slowly)

--- Day06/Program.fs
+++ Day06/Program.fs
@@ -43,3 +43,25 @@ let part1 () =
     |> Set.count
 
 printfn "Part 1: %A" (part1 ())
+
+let rec walksLoop map steps (y, x) (dy, dx) extraObstacle =
+    let (ny, nx) = (y + dy, x + dx)
+    if ny < 0 || Array2D.length1 map <= ny || nx < 0 || Array2D.length2 map <= nx then
+        false
+    elif steps |> Set.contains ((ny, nx), (dy, dx)) then
+        true
+    elif (ny, nx) = extraObstacle ||  map[ny, nx] = '#' then
+        walksLoop map (Set.add ((y, x), (dy, dx)) steps) (y, x) (turn (dy, dx)) extraObstacle
+    else
+        walksLoop map (Set.add ((y, x), (dy, dx)) steps) (ny, nx) (dy, dx) extraObstacle
+
+let part2 () =
+    seq {
+        for (y, x) in walk map Set.empty startPosition startDirection do
+            if map[y, x] = '.' then
+                (y, x)
+    }
+    |> Seq.filter (walksLoop map Set.empty startPosition startDirection)
+    |> Seq.length
+
+printfn "Part 2: %A" (part2 ())

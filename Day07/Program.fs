62c78c5f337f7e27a58392381011857aaaa7e09a
Arthur Rump <hello@arthurrump.com>
2024-12-07 18:12:58 +0100

    Day 7 part 2

--- Day07/Program.fs
+++ Day07/Program.fs
@@ -12,7 +12,7 @@ let equations =
     File.ReadAllLines("input.txt")
     |> Array.map parseLine
 
-let rec possibleResults values =
+let rec possibleResults1 values =
     let first::rest = values
     List.fold (fun results value ->
         Set.union
@@ -23,8 +23,27 @@ let rec possibleResults values =
 let part1 () =
     equations
     |> Array.filter (fun (expected, values) -> 
-        possibleResults values
+        possibleResults1 values
         |> Set.contains expected)
     |> Array.sumBy fst
 
 printfn "Part 1: %A" (part1 ())
+
+let rec possibleResults2 expected values =
+    let first::rest = values
+    List.fold (fun results value ->
+        Set.unionMany [
+            (Set.map (fun res -> res + value) results)
+            (Set.map (fun res -> res * value) results)
+            (Set.map (fun res -> bigint.Parse (string res + string value)) results)
+        ] |> Set.filter (fun res -> res <= expected))
+        (Set.singleton first) rest
+
+let part2 () =
+    equations
+    |> Array.filter (fun (expected, values) -> 
+        possibleResults2 expected values
+        |> Set.contains expected)
+    |> Array.sumBy fst
+
+printfn "Part 2: %A" (part2 ())

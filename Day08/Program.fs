3fd0a43e689c6c1048e236950b9b64f0f75f0c77
Arthur Rump <hello@arthurrump.com>
2024-12-08 10:42:16 +0100

    Day 8 part 2

--- Day08/Program.fs
+++ Day08/Program.fs
@@ -29,9 +29,39 @@ let antinodes antennas =
 
 let part1 () =
     antennas
-    |> List.map (fun (ch, antennas) ->  antinodes antennas)
+    |> List.map (snd >> antinodes)
     |> Set.unionMany
     |> Set.filter inMap
     |> Set.count
 
 printfn "Part 1: %A" (part1 ())
+
+let rec gcd a b =
+    if b = 0
+    then a
+    else gcd b (a % b)
+
+let newAntinodesForPair (y1, x1) (y2, x2) =
+    let dy = y1 - y2
+    let dx = x1 - x2
+    let div = gcd dy dx
+    let dy = dy / div
+    let dx = dx / div
+    let gen step =
+        Seq.initInfinite (fun i -> (y1 + i * dy * step), (x1 + i * dx * step))
+        |> Seq.takeWhile inMap
+    Set.union (set (gen 1)) (set (gen -1))
+
+let newAntinodes antennas =
+    List.allPairs antennas antennas
+    |> List.filter (fun (a1, a2) -> a1 <> a2)
+    |> List.map (fun (a1, a2) -> newAntinodesForPair a1 a2)
+    |> Set.unionMany
+
+let part2 () =
+    antennas
+    |> List.map (snd >> newAntinodes)
+    |> Set.unionMany
+    |> Set.count
+
+printfn "Part 2: %A" (part2 ())

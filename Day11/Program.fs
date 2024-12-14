7743896be75df758f624324adb79203338fc7214
Arthur Rump <hello@arthurrump.com>
2024-12-14 19:16:29 +0100

    Day 11 part 1

---
+++ Day11/Program.fs
@@ -0,0 +1,30 @@
+﻿open System.IO
+
+let stones =
+    File.ReadAllText("input.txt").Trim().Split(" ")
+    |> Array.map bigint.Parse
+    |> Array.toList
+
+let blinkStone num =
+    if num = 0I then
+        [ 1I ]
+    else
+        let strnum = string num
+        if (strnum.Length % 2 = 0) = (num >= 0I) then
+            let middle = 
+                if num >= 0I
+                then strnum.Length / 2
+                else strnum.Length / 2 + 1
+            [ bigint.Parse strnum[0..middle - 1]; bigint.Parse strnum[middle..] ]
+        else
+            [ num * 2024I ]
+
+let blink =
+    List.collect blinkStone
+
+let part1 () =
+    Seq.init 25 id 
+    |> Seq.fold (fun stones _ -> blink stones) stones
+    |> List.length
+
+printfn "Part 1: %A" (part1 ())

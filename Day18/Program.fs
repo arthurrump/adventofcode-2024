7796ac6de5e3853171946183be1010300a406968
Arthur Rump <hello@arthurrump.com>
2024-12-18 21:46:19 +0100

    Attempt at Day 18 part 2 (too slow to work)

--- Day18/Program.fs
+++ Day18/Program.fs
@@ -58,4 +58,62 @@ let part1 () =
     dijkstra start isDest (neighbours corrupted) (fun _ _ -> 1)
     |> fst
 
-printfn "Part 1: %A" (part1 ())
+// printfn "Part 1: %A" (part1 ())
+
+let corruptNeighbours corrupted (x, y) =
+    let neighbours =
+        [ if y > 0 then (x, y - 1)
+          if y > 0 && y < size - 1 then (x + 1, y - 1)
+          if x < size - 1 then (x + 1, y)
+          if y < size - 1 && x < size - 1 then (x + 1, y + 1)
+          if y < size - 1 then (x, y + 1)
+          if y < size - 1 && x > 0 then (x - 1, y + 1)
+          if x > 0 then (x - 1, y)
+          if y > 0 && x > 0 then (x - 1, y - 1) ]
+        |> Set.ofList
+    Set.intersect neighbours corrupted
+
+let corruptStarts =
+    Set.filter (fun (x, y) -> x = size - 1 || y = 0)
+
+let corruptIsDest (x, y) =
+    x = 0 || y = size - 1
+
+let allPaths start isDest neighbours =
+    let rec walk (paths: 'n list list) (frontier: 'n list list) =
+        match frontier with
+        | [] ->
+            paths
+        | next::frontier ->
+            if List.head next |> isDest then
+                walk (next::paths) frontier
+            else
+                let frontier' =
+                    neighbours (List.head next)
+                    |> Seq.filter (fun n -> not (List.contains n next))
+                    |> Seq.map (fun n -> n::next)
+                    |> Seq.toList
+                walk paths (frontier' @ frontier)
+
+    walk [] [ [ start ] ]
+
+// Find all continuous paths (diagonals are continuous) from topright to
+// bottomleft separating the two corners. From each path find the last byte
+// fallen and from those find the first.
+
+let part2 () =
+    let corrupted = set fallingBytes
+    let indices =
+        fallingBytes 
+        |> Array.indexed 
+        |> Array.map (fun (i, pos) -> pos, i)
+        |> Map.ofArray
+    let index pos = Map.find pos indices
+    corruptStarts corrupted
+    |> Seq.collect (fun start -> 
+        allPaths start corruptIsDest (corruptNeighbours corrupted)
+    )
+    |> Seq.map (List.maxBy index)
+    |> Seq.minBy index
+
+printfn "Part 2: %A" (part2 ())
